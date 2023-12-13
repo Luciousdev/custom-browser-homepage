b6cfb5402bc53d7e552239c99e3d7d7e8a7159e0
Luciousdev <lucy@luciousdev.nl>
2023-12-13 14:50:12 +0100

    Refactor background handling and add error handling for API request

--- components/background.js
+++ components/background.js
@@ -1,31 +1,44 @@
-// Get configuration from config.json
+let unsplashAccessKey;
+
+
 $.getJSON('../config.json', function(config) {
     const apiKey = config.apiKey;
     const lat = config.lat;
     const lon = config.lon;
+    unsplashAccessKey = config.unsplashAccessKey;
+    const link = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${apiKey} `;
 
-    // Get weather data 
-    $.getJSON(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`, function(data) {
+    // Get weather data
+    $.getJSON(link, function(data) {
         const weatherCondition = data.weather[0].main.toLowerCase();
+        console.log(data['weather'][0]);
 
         // Update background based on weather condition
         updateBackground(weatherCondition);
     });
+}).fail(function(jqxhr, textStatus, error) {
+    var err = textStatus + ", " + error;
+    console.log("Request Failed: " + err);
 });
 
 function updateBackground(weatherCondition) {
     const body = $('body');
 
-    switch (weatherCondition) {
-        case 'rain':
-            console.log('rainy');
-            // Set background image or color for rainy weather
-            body.css('background', 'url(rainy-background.jpg) no-repeat center center fixed');
-            body.css('background-size', 'cover');
-            break;
-        default:
-            console.log('other: ' + weatherCondition);
-            body.css('background', '#1c1c1c');
-            break;
+    try {
+        fetchImage(weatherCondition)
+            .then(imageUrl => {
+                body.css('background', `url(${imageUrl}) no-repeat center center fixed`);
+                body.css('background-size', 'cover');
+            });
+    } catch (err) {
+        console.log(err);
     }
+}
+
+function fetchImage(keyword) {
+    const unsplashUrl = `https://api.unsplash.com/photos/random?query=${keyword}&orientation=landscape&client_id=${unsplashAccessKey}&featured=true`;
+
+    return fetch(unsplashUrl)
+        .then(response => response.json())
+        .then(data => data.urls.full);
 }
